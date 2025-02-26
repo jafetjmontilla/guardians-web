75ce0dea611ae3ddeacff437c9b3b59cd2dda31e
Jafet Montilla <jafetj@hotmail.com>
2025-02-26 02:52:50 -0400

    added all frames

--- components/ImageComponet.tsx
+++ components/ImageComponet.tsx
@@ -1,22 +1,83 @@
-import { DetailedHTMLProps, FC, HTMLAttributes } from "react"
+import { DetailedHTMLProps, FC, HTMLAttributes, useState } from "react"
+
+type asd = "tl" | "tr" | "bl" | "br"
 
 interface Props {
-  className?: string;
+  label?: string
+  className?: string
+  hover?: string
+  labelPosition?: asd
 }
 
 export const ImageComponets: FC<Props> = (props) => {
+  const [hover, setHover] = useState(false)
 
-  console.log(100031, props.className)
-  const size = 20
+
+
+  const size = 16
   return (
     <>
-      <div className={`bg-white rounded-2xl flex justify-center items-center relative ${props.className}`}  >
-        <div className="absolute left-0 bottom-0 flex items-end">
-          <div>
-            <div className="bg-black triangle-curved rotate-90" />
-            <div className="bg-black rounded-r-2xl rounded-b-none w-[100px] h-[50px]"></div>
+      <div
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        className={`flex justify-center items-center ${props.className}`}  >
+        <div className={`bg-white rounded-2xl relative transition-width transition-height duration-500 ease-linear w-full h-full ${props.hover}`}>
+          <div className={`absolute ${!props?.labelPosition
+            ? "left-0 bottom-0 flex items-end"
+            : props.labelPosition == "tr"
+              ? "top-0 right-0 flex-row-reverse"
+              : props.labelPosition == "tl"
+                ? "top-0 left-0"
+                : props.labelPosition == "br"
+                  ? "bottom-0 right-0 flex-row-reverse items-end" :
+                  "bottom-0 right-0 flex items-end"
+            }  flex  `}>
+            <div className={`flex  ${!props?.labelPosition
+              ? "flex-col"
+              : props.labelPosition == "tr"
+                ? "items-end flex-col-reverse"
+                : props.labelPosition == "tl"
+                  ? "flex-col-reverse"
+                  : props.labelPosition == "br"
+                    ? "items-end flex-col" :
+                    "flex-col"
+              }`}>
+              <div className={`bg-black triangle-curved ${!props?.labelPosition
+                ? "rotate-90"
+                : props.labelPosition == "tr"
+                  ? "-rotate-90"
+                  : props.labelPosition == "tl"
+                    ? "rotate-180"
+                    : props.labelPosition == "br"
+                      ? "" :
+                      "rotate-90"
+                }`} />
+              <div className={`bg-black *w-[100px] *h-[50px] transition-width transition-height duration-500 ease-linear flex items-center justify-center ${hover && "*w-[120px] *h-[60px]"} ${!props?.labelPosition
+                ? "rounded-tr-2xl"
+                : props.labelPosition == "tr"
+                  ? "rounded-bl-2xl"
+                  : props.labelPosition == "tl"
+                    ? "rounded-br-2xl"
+                    : props.labelPosition == "br"
+                      ? "rounded-tl-2xl" :
+                      "rounded-tr-2xl"
+                }`}>
+                <span className={`transition-text duration-500 ease-linear text-white p-4 text-sm ${hover && "*scale-110 text-xl text-orange-400"}`}>
+                  {props.label}
+                </span>
+              </div>
+            </div>
+            <div className={`bg-black triangle-curved ${!props?.labelPosition
+              ? "rotate-90"
+              : props.labelPosition == "tr"
+                ? "-rotate-90"
+                : props.labelPosition == "tl"
+                  ? "rotate-180"
+                  : props.labelPosition == "br"
+                    ? "" :
+                    "rotate-90"
+              }`} />
           </div>
-          <div className="bg-black triangle-curved rotate-90" />
         </div>
 
 
--- pages/home.tsx
+++ pages/home.tsx
@@ -4,10 +4,26 @@ import { TestFigure } from "@/components/TestFigure"
 
 function Home() {
   return (
-    <div className="flex w-full h-full gap-3">
-      <ImageComponets className="w-[300px] h-[150px]" />
-      <ImageComponets className="w-[400px] h-[150px]" />
-      <ImageComponets className="flex-1 h-[150px]" />
+    <div className="flex w-full h-full gap-4 p-4">
+      <div className="flex flex-col w-[55vw] h-full gap-4">
+        <ImageComponets label="Leyenda uno" className="w-full h-[68%]" hover="hover:w-[95%] hover:h-[95%]" />
+
+        <div className="flex w-full flex-1 gap-4">
+          <ImageComponets label="Leyenda dos" labelPosition="tr" className="flex w-[50%] h-full" hover="hover:w-[95%] hover:h-[95%]" />
+          <ImageComponets label="Leyenda tres" labelPosition="br" className="flex w-[50%] h-full" hover="hover:w-[95%] hover:h-[95%]" />
+
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col h-full gap-4">
+        <div className="w-full h-[68%] gap-4 flex flex-col">
+          <ImageComponets label="Leyenda cuatro" className="w-full h-[56%]" hover="hover:w-[95%] hover:h-[95%]" />
+          <div className="w-full flex-1 flex gap-4">
+            <ImageComponets label="Leyenda cinco" className="w-1/2 h-full" hover="hover:w-[95%] hover:h-[95%]" />
+            <ImageComponets label="Leyenda seis" labelPosition="tr" className="w-1/2 h-full" hover="hover:w-[95%] hover:h-[95%]" />
+          </div>
+        </div>
+        <ImageComponets label="Leyenda siete" labelPosition="tr" className="w-full flex-1" hover="hover:w-[95%] hover:h-[95%]" />
+      </div>
     </div>
   )
 }
