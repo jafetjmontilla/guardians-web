b9b41eefff7365f2e296dcc39b097c62c15210f2
Jafet Montilla <jafetj@hotmail.com>
2025-02-25 22:00:53 -0400

    working in Frame

--- components/ImageComponet.tsx
+++ components/ImageComponet.tsx
@@ -7,11 +7,29 @@ interface Props {
 export const ImageComponets: FC<Props> = (props) => {
 
   console.log(100031, props.className)
-
+  const size = 20
   return (
-    <div className={`bg-white rounded-3xl flex justify-center items-center ${props.className}`}  >
-      {yoSoyJson.nombre}
-    </div>
+    <>
+      <div className={`bg-white rounded-2xl flex justify-center items-center relative ${props.className}`}  >
+        <div className="absolute left-0 bottom-0 flex items-end">
+          <div>
+            <div className="bg-black triangle-curved rotate-90" />
+            <div className="bg-black rounded-r-2xl rounded-b-none w-[100px] h-[50px]"></div>
+          </div>
+          <div className="bg-black triangle-curved rotate-90" />
+        </div>
+
+
+        {/* {yoSoyJson.nombre} */}
+      </div>
+      <style jsx>{`
+        .triangle-curved {
+          width: ${size}px;
+          height: ${size}px;
+          clip-path: path("M 0 ${size} L 0 ${size} C 0 ${size}, ${size} ${size}, ${size} 0 L ${size} 0 L ${size} ${size} z");
+        }
+      `}</style>
+    </>
   )
 }
 
--- pages/home.tsx
+++ pages/home.tsx
@@ -4,12 +4,10 @@ import { TestFigure } from "@/components/TestFigure"
 
 function Home() {
   return (
-    <div className="flex justify-center items-center h-full gap-3">
-      <TestFigure />
-      <InputBasic className="bg-red-500" value="" />
+    <div className="flex w-full h-full gap-3">
       <ImageComponets className="w-[300px] h-[150px]" />
-      <ImageComponets className="w-[400px] h-[200px]" />
-      <ImageComponets className="w-[120px] h-[80px]" />
+      <ImageComponets className="w-[400px] h-[150px]" />
+      <ImageComponets className="flex-1 h-[150px]" />
     </div>
   )
 }
